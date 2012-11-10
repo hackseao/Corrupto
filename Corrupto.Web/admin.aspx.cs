6a46b028af9eb848178e39d69413de6996fa748a
Guillaume Roy <guillaumeroy@guillaumeroy.com>
2012-11-10 14:40:41 -0500

    !* testing local file access through Azure

--- Corrupto.Web/admin.aspx.cs
+++ Corrupto.Web/admin.aspx.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.IO;
 
 using Corrupto.Logic;
 
@@ -11,6 +12,8 @@ public partial class admin : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        string appRoot = Path.Combine(Environment.GetEnvironmentVariable("RoleRoot") + @"\", @"approot\");
+        Response.Write(appRoot);
     }
 
     protected void btnExecute_Click(object sender, EventArgs e)
